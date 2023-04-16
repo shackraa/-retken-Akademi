fa8d201cbc578976093d64dde4addaf6eb95a146
Ben Atakan <95279470+shackraa@users.noreply.github.com>
2023-04-16 22:00:54 +0300

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <h2>
+      Üretken Akademi
+      <a href='https://www.youtube.com/channel/UCCucrKUVwzzCzMsbGoeyAmQ' target="__blank">Diğer Projelerimi Görmek İçin Tıklayın!</a>
+    </h2>
+  )
+}
